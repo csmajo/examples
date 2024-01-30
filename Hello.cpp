0964109f18aba24e1c838bec3f655b8138c0682c
Chad <chad@Chads-MacBook-Pro.local>
2024-01-30 18:16:08 -0500

    Added First Program

---
+++ Hello.cpp
@@ -0,0 +1,17 @@
+/**
+ *        @file: Hello.cpp
+ *      @author: Nasseef Abukamail
+ *        @date: January 30, 2024
+ *       @brief: Add Description
+ */
+
+#include <iostream>
+using namespace std;
+
+///function prototypes
+
+int main(int argc, char const *argv[]) {
+
+   cout << "Hello World" << endl;
+   return 0;
+}// main
